2ce8f979205e1aa9853eb668281b5be25c52e365
Wade Barnes <wade.barnes@shaw.ca>
2018-05-11 13:23:54 -0700

    Add Jenkins pipeline and OpenShift settings.

---
+++ openshift/settings.sh
@@ -0,0 +1,7 @@
+export PROJECT_NAMESPACE=${PROJECT_NAMESPACE:-devex-von-image}
+export GIT_URI=${GIT_URI:-"https://github.com/PSPC-SPAC-buyandsell/von-image.git"}
+export GIT_REF=${GIT_REF:-"master"}
+
+# The project components
+# - They are all contained under the main OpenShift folder.
+export components="."
